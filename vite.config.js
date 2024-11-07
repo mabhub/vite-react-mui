3166dc115d0f3fd40356b395363b1eeff7501480
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2024-11-07 23:44:06 +0100

    Enable HTTPS by default with local certificate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+
+const key = fs.readFileSync(path.join(os.homedir(), 'https', 'key.pem'));
+const cert = fs.readFileSync(path.join(os.homedir(), 'https', 'cert.pem'));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: { key, cert },
+  },
 });
